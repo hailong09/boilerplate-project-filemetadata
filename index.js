256c55d076097e0b8081aab68d7e583c07ef2e55
Hai Long Do <long2qw@gmail.com>
2024-10-28 20:34:33 -0400

    done project

--- index.js
+++ index.js
@@ -1,15 +1,45 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+const bodyparser = require('body-parser');
+
 require('dotenv').config()
+
+
+
 
 var app = express();
 
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/uploads', express.static(process.cwd() + '/uploads'))
+app.use(bodyparser.urlencoded({ extended: true }))
+app.use(bodyparser.json())
+
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse', upload.single('upfile'), async (req, res) => {
+  try {
+    const { originalname, mimetype, size } = req.file
+    console.log(req.file)
+
+
+    return res.json({
+      name: originalname,
+      type: mimetype,
+      size: size
+    })
+
+  } catch (err) {
+    console.error(err)
+  }
+})
+
 
 
 
